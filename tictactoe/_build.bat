5f0fa5afb8e43ac98ff0bdec96eb2651cf85e2bb
beef <github@bearpaw7.com>
2014-12-13 22:11:23 -0800

    Initial app

---
+++ tictactoe/_build.bat
@@ -0,0 +1,11 @@
+@REM Build the app using jMonkey IDE tools
+@SET local
+
+@CD %~dp0
+
+@SET ANT_HOME=E:\x64_Programs\jmonkeyplatform\java\ant
+@SET PATH=E:\x64_Programs\jmonkeyplatform\jdk\bin;$PATH
+
+@CALL E:\x64_Programs\jmonkeyplatform\java\ant\bin\ant.bat jar
+
+@PAUSE
---
+++ tictactoe/_run.bat
@@ -0,0 +1,11 @@
+@REM Execute the app using jMonkey IDE tools
+@SET local
+
+@CD %~dp0
+
+@SET JAVA_HOME=E:\x64_Programs\jmonkeyplatform\jdk
+
+
+@CALL E:\x64_Programs\jmonkeyplatform\jdk\bin\java -jar "C:\DirtyAnaconda\tictactoe\dist\MyGame.jar"
+
+@PAUSE
